52f4cf66bb3698e845536d259e2b1fe5a9384dc5
lydat1502 <dat.ly.cit23@eiu.edu.vn>
2024-11-29 14:12:17 +0700

    Update print-name

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Dat’s Java Repository.");
+        System.out.println("Hello Dat\nWelcome to my repository.");
     }
 }
